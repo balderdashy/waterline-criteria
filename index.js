ea90e1305457a7b7f7f5bd30f300e7ba6a35fd63
Mike McNeil <mike@balderdash.co>
2016-10-10 14:37:21 -0500

    Set up WHERE validator and stubbed SORT validator.  Also simplify the way stuff gets exported a bit.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib');
+// This package exports a function.  But it also exposes a few other functions
+// as properties of the main function.  See `lib/index.js` for details.
+// (We do this in lib/index.js purely for clarity-- because if we added
+//  the other properties here, it would mutate the function globally anyway,
+//  even if the function was required directly from `lib/index` instead of here.)
+module.exports = require('./lib/index');
